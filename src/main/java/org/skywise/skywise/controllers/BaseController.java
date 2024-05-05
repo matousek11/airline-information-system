b8e543b9eb141b29af8cdc37c9812126b6f968c7
Lukas Matousek <matousek1199@gmail.com>
2024-05-05 11:44:44 +0200

    feat: overview page

--- src/main/java/org/skywise/skywise/controllers/BaseController.java
+++ src/main/java/org/skywise/skywise/controllers/BaseController.java
@@ -1,12 +1,37 @@
 package org.skywise.skywise.controllers;
 
+import org.skywise.skywise.repositories.FlightRepository;
+import org.skywise.skywise.repositories.PlaneRepository;
+import org.skywise.skywise.repositories.RepairRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BaseController {
+    private final FlightRepository flightRepository;
+    private final RepairRepository repairRepository;
+    private final PlaneRepository planeRepository;
+
+    @Autowired
+    public BaseController(
+            FlightRepository flightRepository,
+            RepairRepository repairRepository,
+            PlaneRepository planeRepository
+    ) {
+        this.flightRepository = flightRepository;
+        this.repairRepository = repairRepository;
+        this.planeRepository = planeRepository;
+    }
+
+
     @GetMapping("/overview")
-    public String overview() {
+    public String overview(Model model) {
+        long currentTimestamp = System.currentTimeMillis() / 1000;
+        model.addAttribute("flights", flightRepository.findFlightsDepartingInFuture(currentTimestamp));
+        model.addAttribute("repairs", repairRepository.findAll());
+        model.addAttribute("planes", planeRepository.findAll());
         return "overview";
     }
 
--- src/main/java/org/skywise/skywise/repositories/FlightRepository.java
+++ src/main/java/org/skywise/skywise/repositories/FlightRepository.java
@@ -2,6 +2,11 @@ package org.skywise.skywise.repositories;
 
 import org.skywise.skywise.models.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.ArrayList;
 
 public interface FlightRepository extends JpaRepository<Flight, Long> {
+    @Query("SELECT f FROM Flight f WHERE f.fromTime > ?1")
+    ArrayList<Flight> findFlightsDepartingInFuture(long currentTimestamp);
 }
